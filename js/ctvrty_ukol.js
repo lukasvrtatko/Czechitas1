9ed6f4219b77f61decabba0f3c878ece273b8958
lukasvrtatko <lukas.vrtatko@seznam.cz>
2022-01-22 15:54:55 +0100

    1 file modified

--- js/ctvrty_ukol.js
+++ js/ctvrty_ukol.js
@@ -3,10 +3,38 @@
   - Math.abs()
 */
 
-console.log('ctvrty ukol nahrán')
 
 
+function VypisMatematickeOperace[x,y]{
+  let soucet = x + y
+  let rozdíl = Math.abs(x - y)
+  let násobek = x * y
+  let podíl= x / y
 
+console.log('Soucet: ' + soucet +
+' Rozdíl: ' + rozdíl+
+' Násobek: ' + násobek +
+' Podíl: ' + podíl
+)
+}
+
+function spoctiObsahObdelniku(x,y){
+  let vyseldek = x * y 
+
+  return vysledek
+}
+
+let x = Number(promt('Zadej X:'))
+let y = Number(promt('Zadej Y:'))
+
+vypisMatematickeOperace(x,y)
+//A:
+console.log('Obsah obdelniku je:' + spoctiObsahObdelniku(x,y))
+//B:
+let matOperce =spoctiO,bsahObdelniku(x,y) + spoctiObsahObdelniku(18,15)
+console.log(VypisMatematickeOperace)
+
+let obsahObdelniku = spoctiObsahObdelniku(x,y) // ->
 
 
 /*
@@ -15,3 +43,15 @@ console.log('ctvrty ukol nahrán')
   zbytek po dělení: % - modulo​
   - v prvni moment predpokladejme, ze cislo JE prvocislo a budeme pripadne rosporovat toto tvrzeni.
 */
+
+
+function jePrvocislo(x) {
+  if(x % 2 === 1) {
+    return('Je liché - ')
+  }else{
+    return('Je sudé - ')
+  }
+}
+
+let cislo = jePrvocislo(Number(promt('Zadej cislo:')))
+console.log(cislo)
